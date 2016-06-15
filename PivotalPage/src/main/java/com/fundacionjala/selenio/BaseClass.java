2f4d258e8199393656a926e963c48ef251f408c1
MijhailVillarroel <Mijhail.Villarroel@fundacion-jala.org>
2016-06-15 17:32:56 -0400

    Update BaseClass.java

--- PivotalPage/src/main/java/com/fundacionjala/selenio/BaseClass.java
+++ PivotalPage/src/main/java/com/fundacionjala/selenio/BaseClass.java
@@ -15,30 +15,11 @@ public abstract class BaseClass {
 
     private static final int TIMEOUT_NORMAL = 15;
 
-    private static WebDriver instance;
-
-    private WebDriverWait wait;
-
     public BaseClass() {
         driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-
-        wait = new WebDriverWait(driver, TIMEOUT_NORMAL);
+        driver.manage().timeouts().implicitlyWait(TIMEOUT_NORMAL, TimeUnit.SECONDS);
     }
-
-    public static BaseClass getInstance() {
-        if (instance == null) {
-            instance = new WebDriverManager();
-        }
-        return instance;
-    }
-
-    public WebDriver getDriver() {
-        return driver;
-    }
-
-    public WebDriverWait getWait() {
-        return wait;
-    }
-
 }
+
+
+
